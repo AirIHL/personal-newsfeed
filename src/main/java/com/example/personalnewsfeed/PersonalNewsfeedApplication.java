c3814ee9cf13391d76e2f7f5cc19ed07a3806ca8
AirIHL <inhak1122@gmail.com>
2025-03-10 23:08:25 +0900

    first commit

---
+++ src/main/java/com/example/personalnewsfeed/PersonalNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalNewsfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalNewsfeedApplication.class, args);
+    }
+
+}
